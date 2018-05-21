8a62131802ed6552e554d3fce5695ccc074aafb2
Kishan <kvikani@pltw.com>
2018-05-21 20:41:36 +0530

    Added a simple Login Form

--- client/forum-client/src/App.js
+++ client/forum-client/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './LoginForm'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return (   
+<div className="container">
+  <div className="row">
+    <div className="col-lg offset-4">
+  <div className="login-container">
+      <LoginForm/>
+    </div>    
+    </div>
+  </div>
+</div>
     );
   }
 }
---
+++ client/forum-client/src/LoginForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const LoginForm = () =>{
+    return(
+        <form>
+        <div className="form-group">
+           <label htmlFor="exampleInput1" className="bmd-label-floating">Email</label>
+           <input type="email" className="form-control" id="exampleInput1"/>           
+        </div>
+        <div className="form-group">
+        <div className="form-group">
+           <label htmlFor="exampleInput2" className="bmd-label-floating">Password</label>
+           <input type="password" className="form-control" id="exampleInput2"/>           
+        </div>
+        </div>
+        <div className="form-check">
+            <label className="form-check-label">
+                <input className="form-check-input" type="checkbox" value=""/>
+                Remember Me
+                <span className="form-check-sign">
+                    <span className="check"></span>
+                </span>
+            </label>
+        </div>
+      
+        <button primary 
+        type="submit" className="btn btn-primary">Login</button>
+      </form>
+    );
+};
+
+export default LoginForm;
+
